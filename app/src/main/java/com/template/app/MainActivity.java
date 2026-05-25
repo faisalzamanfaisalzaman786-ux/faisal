97c207b0bcbdc49d07e07e1e72cfe295c5e23d9f
faisalzamanfaisalzaman786-ux <faisalzamanfaisalzaman786@gmail.com>
2026-05-25 19:55:27 +0500

    Update placeholders for com.faisal.app

--- app/src/main/java/com/template/app/MainActivity.java
+++ app/src/main/java/com/template/app/MainActivity.java
@@ -1,4 +1,4 @@
-package {{PACKAGE_NAME}};
+package com.faisal.app;
 
 import android.os.Bundle;
 import android.widget.TextView;
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         TextView tv = new TextView(this);
-        tv.setText("Hello from {{APP_NAME}}!");
+        tv.setText("Hello from faisal!");
         tv.setTextSize(24);
         setContentView(tv);
     }
